32c3c599125f28347aaf640bbd4b2d723f7a52be
khalidmomar <khalidomarasu@gmail.com>
2022-07-28 15:19:52 +0300

    added new model funcs

--- model.py
+++ model.py
@@ -1,3 +1,13 @@
 
 
 print("Adding all ML code")
+
+def model1():
+    print("adding code for ML model 1")
+
+def model2():
+    print("adding code for ML model 2")
+
+
+def model3():
+    print("adding code for ML model 3")
